45e519295959c1d602b618b09cece662aa1dd3a1
JcbSm <jacob.ted.seaman@gmail.com>
2020-11-30 19:12:46 +0000

    Update ready.js

--- listeners/ready.js
+++ listeners/ready.js
@@ -10,6 +10,19 @@ class ReadyListener extends Listener {
 
     async exec() {
         console.log('Online')
+        
+        //Resetting things.
+        const voiceMembers = (await this.client.db.query(`SELECT guild_id, user_id FROM members WHERE voice = true`)).rows;
+
+        for(let i = 0; i < voiceMembers.length; i++) {
+            let member = await (await this.client.guilds.fetch(voiceMembers[i].guild_id)).members.fetch(voiceMembers[i].user_id);
+            if(member.voice.channel) {
+                this.client.emit('xp-joinVoice', null, member.voice)
+                this.client.emit('stats-joinVoice', null, member.voice)
+            } else {
+                this.client.db.query(`UPDATE members SET voice = false WHERE user_id = ${member.id} AND guild_id = ${member.guild.id}`)
+            };
+        };
     };
 };
 

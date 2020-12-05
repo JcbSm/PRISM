88dfa95ac61607281d8666572a03ff4ad8ff2fda
JcbSm <jacob.ted.seaman@gmail.com>
2020-12-05 22:54:19 +0000

    Testing Inhibitor Added

---
+++ Inhibitors/testing.js
@@ -0,0 +1,16 @@
+const { Inhibitor } = require('discord-akairo');
+
+class TestingInhibitor extends Inhibitor {
+    constructor() {
+        super('testing', {
+            reason: 'testing'
+        });
+    };
+
+    exec(message) {
+
+        return (message.guild.id !== '569556194612740115');
+    };
+};
+
+module.exports = TestingInhibitor;

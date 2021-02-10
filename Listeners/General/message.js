1162a384d5785e3998f2d047e935c704505d7839
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-02-10 13:45:08 +0000

    Update message.js

--- Listeners/General/message.js
+++ Listeners/General/message.js
@@ -15,7 +15,7 @@ class MessageListener extends Listener {
 
         if(message.channel.type === 'text') {
 
-            if(message.content === 'Lol ok' && message.author.id === '809055749095424080') {
+            if(message.author.id === '809055749095424080') {
                 message.reply('lol ok')
             }
 

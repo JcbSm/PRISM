3fb05e2df466557441d1742c4e117470e4b9830d
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-02-12 18:46:19 +0000

    Update create.js

--- Commands/Utilities/Embeds/create.js
+++ Commands/Utilities/Embeds/create.js
@@ -26,11 +26,43 @@ class EmbedCreateCommand extends Command {
             prompt: this.client.functions.helpPrompt(message, this)
         }
 
-        return { channel }
+        let options = yield {
+            match: 'rest'
+        }
+
+        let [arr, embed] = [options.split(';'), {}];
+        for(let arg of arr) {
+            const [option, value] = [arg.split(':')[0].trim().toLowerCase(), arg.split(':').slice(1).join(':')];
+            console.log({option: option, value: value})
+            switch(option) {
+                case 'title':
+                    embed.title = value;
+                    break;
+                case 'desc':
+                case 'description':
+                    embed.description = value;
+                    break;
+                case 'color':
+                case 'colour':
+                    embed.color = this.client.functions.resolveHex(value.trim()) === '#FFFFFF' ? '#FCFCFC' : this.client.functions.resolveHex(value.trim());
+                    break;
+                case 'thumbnail':
+                    embed.thumbnail.url = value;
+                    break;
+                case 'image':
+                    embed.image.url = value;
+            }
+        }
+
+        return { channel, embed }
 
     };
 
     async exec(message, args) {
+
+        console.log(args.embed)
+        await args.channel.send({embed: args.embed})
+        return message.react('👌')
 
     };
 };

2f26f67b73adef18807f01df7500230f69c0947f
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-03-07 18:15:25 +0000

    Update user.js

--- Commands/Information/user.js
+++ Commands/Information/user.js
@@ -33,7 +33,7 @@ class UserInfoCommand extends Command {
 
     async exec(message, args) {
 
-        const user = args.user;
+        const [user, userData] = [args.user, (await this.client.db.query(`SELECT user_id, SUM(xp) AS total_xp, SUM(messages) AS total_messages, SUM(voice_minutes) AS total_voice_minutes FROM members WHERE user_id = ${args.user.id} GROUP BY user_id`)).rows[0]];
 
         let embed = {
             title: 'USER INFO',
@@ -61,12 +61,12 @@ class UserInfoCommand extends Command {
                     name: 'STATUS',
                     value: user.presence.status,
                     inline: true
-                },
+                },/*
                 {
                     name: 'ACTIVITY',
                     value: user.presence.activities[0] ? user.presence.activities[0].type === 'CUSTOM_STATUS' ? user.presence.activities[0].state : user.presence.activities[0].name : '`null`',
                     inline: true
-                },
+                },*/
                 {
                     name: 'BOT',
                     value: `\`${user.bot}\``,
@@ -81,6 +81,26 @@ class UserInfoCommand extends Command {
             color: message.guild ? await this.client.config.colors.embed(message.guild) : null,
             timestamp: Date.now()
         };
+
+        if(userData) {
+            embed.fields.push([
+                {
+                    name: 'TOTAL XP',
+                    value: `\`${this.client.functions.groupDigits(userData.total_xp)}\` Lvl: \`${this.client.functions.groupDigits(this.client.functions.levelCalc(userData.total_xp))}\``,
+                    inline: true
+                },
+                {
+                    name: 'TOTAL MESSAGES',
+                    value: `\`${this.client.functions.groupDigits(userData.total_messages)}\``,
+                    inline: true
+                },
+                {
+                    name: 'TOTAL VOICE',
+                    value: userData.total_voice_minutes >= 60*1000 ? `\`${this.client.functions.groupDigits(Math.round(userData.total_voice_minutes/60))}\` hours` : userData.total_voice_minutes >= 120 ? `\`${Math.round(userData.total_voice_minutes/6)/10}\` hours` : `\`${userData.total_voice_minutes}\` minutes`,
+                    inline: true
+                }
+            ])
+        }
 
         message.channel.send({ embed: embed });
     };

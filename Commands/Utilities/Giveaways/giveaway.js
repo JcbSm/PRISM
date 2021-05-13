235b473e2fb59f2c418246286f1874b6d30307d6
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-05-13 14:01:29 +0100

    Create giveaway.js

---
+++ Commands/Utilities/Giveaways/giveaway.js
@@ -0,0 +1,48 @@
+const { Command } = require('discord-akairo');
+const { commandOptions } = require('../../../index');
+
+const commandInfo = commandOptions({
+    id: 'giveaway',
+    aliases: [],
+    description: {
+        usage: ['[prize]'],
+        content: 'Giveaway a prize'
+    },
+    channel: 'guild',
+    typing: false,
+    clientPermissions: ['SEND_MESSAGES'],
+    userPermissions: []
+}, __dirname);
+
+class GiveawayCommand extends Command {
+    constructor() {
+        super(commandInfo.id, commandInfo);
+    };
+
+    async *args(message) {
+
+        const prize = yield {
+            type: 'string',
+            match: 'rest',
+            prompt: this.client.functions.helpPrompt(message, this)
+        }
+
+        return { prize }
+
+    };
+
+    async exec(message, args) {
+
+        let sent = await message.channel.send({ embed: {
+            title: 'GIVEAWAY',
+            description: `\`\`\`${args.prize}\`\`\`\nReact with 🎉 to enter.`,
+            color: await this.client.config.colors.embed(message.guild),
+            timestamp: Date.now()
+        }});
+
+        await sent.react('🎉')
+
+    };
+};
+
+module.exports = GiveawayCommand;

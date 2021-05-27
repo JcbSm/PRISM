aa7393245f9cfe5d4649a9aeeac6399bddd74735
JcbSm <49797894+JcbSm@users.noreply.github.com>
2021-05-27 20:26:11 +0100

    Update xp.js

--- Commands/Info/Stats/xp.js
+++ Commands/Info/Stats/xp.js
@@ -40,7 +40,7 @@ class XpCommand extends Command {
         const timeSince = new Date(message.createdTimestamp - xp_last_message_timestamp);
         
         return message.channel.send({ files: [{ attachment: await this.client.getRankCard(args.member), name: 'rank.png' }], embed: {
-            title: `EXPERIENCE`,
+            title: `EXPERIENCE`, // `LEVEL [${level}]`,
             //description: `\`\`\`${groupDigits(current)} / ${groupDigits(max-min)}\`\`\``,
             fields: [
                 {
